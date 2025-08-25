48830b47ca5dd282fcaeedbdf1b3fdc2a3e54bc4
SV-Alexey <sultanovalexey@mail.ru>
2025-08-25 08:39:12 +0500

    1

--- cats.py
+++ cats.py
@@ -1,1 +1,20 @@
-## asdwd
+from tkinter import *
+from PIL import ImageTk, Image
+import requests
+from io import BytesIO
+
+window = tk()
+window.title("Котики")
+window.geometry("600x480")
+
+label = Label()
+label.pack()
+
+url = "http://cataas.com/cat"
+img = load_image(url)
+
+if img:
+    label.config(image=img)
+    label.image = img
+
+window.mainloop()
