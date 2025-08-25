3d47aa273e850512fa63f124dc4a508adab0a8ce
SV-Alexey <sultanovalexey@mail.ru>
2025-08-25 09:06:13 +0500

    1

--- cats.py
+++ cats.py
@@ -10,24 +10,39 @@ def load_image(url):
         response.raise_for_status()
         img_data = BytesIO(response.content)
         img = Image.open(img_data)
+        img.thumbnail((600, 480), Image.Resampling.LANCZOS)
         return ImageTk.PhotoImage(img)
     except Exception as e:
         print(f"Произошла ошибка {e}")
         return None
 
 
+def set_image():
+    img = load_image(url)
+
+    if img:
+        label.config(image=img)
+        label.image = img
+
 window = Tk()
-window.title("Котики")
-window.geometry("600x480")
+window.title("Котики!")
+window.geometry("600x520")
 
 label = Label()
 label.pack()
 
-url = "http://cataas.com/cat"
-img = load_image(url)
+# updete_button = Button(text="Обновить", command=set_image)
+# updete_button.pack()
 
-if img:
-    label.config(image=img)
-    label.image = img
+menu_bar = Menu(window)
+window.config(menu=menu_bar)
+
+file_menu = Menu(menu_bar, tearoff=0)
+menu_bar.add_cascade(label="Файл", menu=file_menu)
+file_menu.add_command(label="Обновить", command=set_image)
+
+url = "http://cataas.com/cat"
+
+set_image()
 
 window.mainloop()
